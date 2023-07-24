69285a2578ad10418d851aa12135ec0921c8c7e0
devucb <91523007+devucb@users.noreply.github.com>
2023-07-24 16:54:12 +0300

    fix: vector-icon setup

--- App.tsx
+++ App.tsx
@@ -7,9 +7,18 @@
 
 import React from 'react';
 import './src/locale/i18n';
+import {Button} from 'react-native-paper';
 import {RootNavigationProvider} from '@routes';
 function App(): JSX.Element {
-  return <RootNavigationProvider />;
+  return (
+    <Button
+      rippleColor="#FF000020"
+      icon="camera"
+      mode="contained"
+      onPress={() => console.log('Pressed')}>
+      Press me
+    </Button>
+  );
 }
 
 export default App;
